40d05bb8ca1ff97c49b2030d96ebf51286d4dae5
Kevin <jjimenezbal@gmail.com>
2023-03-20 15:37:51 -0600

    Leccion 110
    Creacion de las peliculas api

---
+++ back-end/Controllers/PeliculasController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using back_end.DTO;
+using back_end.Entidades;
+using back_end.Utilidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    public class PeliculasController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+        private readonly IAlmacenadorArchivos almacenadorArchivos;
+        private readonly string contenedor = "peliculas";
+
+        public PeliculasController(ApplicationDbContext context, IMapper mapper, IAlmacenadorArchivos almacenadorArchivos)
+        {
+            this.context = context;
+            this.mapper = mapper;
+            this.almacenadorArchivos = almacenadorArchivos;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromForm] PeliculaCreacionDTO peliculaCreacionDTO)
+        {
+            var pelicula = mapper.Map<Pelicula>(peliculaCreacionDTO);
+
+            if (peliculaCreacionDTO.Poster != null)
+            {
+                pelicula.Poster = await almacenadorArchivos.GuadarArchivo(contenedor, peliculaCreacionDTO.Poster);
+            }
+            EscribirOrdenActores(pelicula);
+            context.Add(pelicula);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private void EscribirOrdenActores(Pelicula pelicula)
+        {
+            if(pelicula.PeliculasActores != null)
+            {
+                for(int i = 0; i < pelicula.PeliculasActores.Count; i++)
+                {
+                    pelicula.PeliculasActores[i].Orden = i;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ back-end/DTO/ActorPeliculaCreacionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace back_end.DTO
+{
+    public class ActorPeliculaCreacionDTO
+    {
+        public int Id { get; set; }
+        public string Personaje { get; set; }    
+    }
+}
