ad3da1caa439a7c4576086f052822060f5c3d2a3
Kevin <jjimenezbal@gmail.com>
2023-03-20 16:13:51 -0600

    Leccion 111
    Actualizacion creacion de peliculas

--- back-end/Controllers/PeliculasController.cs
+++ back-end/Controllers/PeliculasController.cs
@@ -3,10 +3,12 @@ using back_end.DTO;
 using back_end.Entidades;
 using back_end.Utilidades;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace back_end.Controllers
 {
-    public class PeliculasController: ControllerBase
+    [Route("api/peliculas")]
+    public class PeliculasController : ControllerBase
     {
         private readonly ApplicationDbContext context;
         private readonly IMapper mapper;
@@ -35,11 +37,24 @@ namespace back_end.Controllers
             return NoContent();
         }
 
+        [HttpGet("postget")]
+        public async Task<ActionResult<PeliculasPostGetDTO>> PosGet()
+        {
+            var cines = await context.Cines.ToListAsync();
+            var generos = await context.Generos.ToListAsync();
+
+            var cinesDTO = mapper.Map<List<CineDTO>>(cines);
+            var generosDTO = mapper.Map<List<GeneroDTO>>(generos);
+
+            return new PeliculasPostGetDTO() { Cines = cinesDTO, Generos = generosDTO };
+
+        }
+
         private void EscribirOrdenActores(Pelicula pelicula)
         {
-            if(pelicula.PeliculasActores != null)
+            if (pelicula.PeliculasActores != null)
             {
-                for(int i = 0; i < pelicula.PeliculasActores.Count; i++)
+                for (int i = 0; i < pelicula.PeliculasActores.Count; i++)
                 {
                     pelicula.PeliculasActores[i].Orden = i;
                 }
