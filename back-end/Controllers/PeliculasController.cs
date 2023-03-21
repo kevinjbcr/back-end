4b0bf4eb4cb183586dd2efbf457083b1c15cf1c2
Kevin <jjimenezbal@gmail.com>
2023-03-20 22:47:28 -0600

    Leccion 116

--- back-end/Controllers/PeliculasController.cs
+++ back-end/Controllers/PeliculasController.cs
@@ -65,6 +65,30 @@ namespace back_end.Controllers
             return dto;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<LandingPageDTO>> Get() 
+        {
+            var top = 6;
+            var hoy = DateTime.Today;
+
+            var proximosEstrenos = await context.Peliculas
+                .Where(x => x.FechaLanzamiento > hoy)
+                .OrderBy(x => x.FechaLanzamiento)
+                .Take(top)
+                .ToListAsync();
+            
+            var enCines = await context.Peliculas
+                .Where(x => x.EnCines)
+                .OrderBy(x => x.FechaLanzamiento)
+                .Take(top)
+                .ToListAsync();
+
+            var resultado = new LandingPageDTO();
+            resultado.ProximosEstrenos = mapper.Map<List<PeliculaDTO>>(proximosEstrenos);
+            resultado.EnCines = mapper.Map<List<PeliculaDTO>>(enCines);
+            return resultado;
+        }
+
         private void EscribirOrdenActores(Pelicula pelicula)
         {
             if (pelicula.PeliculasActores != null)
---
+++ back-end/DTO/LandingPageDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace back_end.DTO
+{
+    public class LandingPageDTO
+    {
+        public List<PeliculaDTO> EnCines { get; set; }
+        public List<PeliculaDTO> ProximosEstrenos { get; set; }
+    }
+}
