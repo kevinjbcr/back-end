e849dba816c76489451979140f81d4943d27a697
Kevin <jjimenezbal@gmail.com>
2023-03-25 18:03:42 -0600

    Leccion 127
    Creando el controlador de cuentas

---
+++ back-end/Controllers/CuentasController.cs
@@ -0,0 +1,81 @@
+﻿using back_end.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace back_end.Controllers
+{
+    [Route("api/cuentas")]
+    [ApiController]
+    public class CuentasController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public CuentasController(UserManager<IdentityUser> userManager, IConfiguration configuration, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost("crear")]
+        public async Task<ActionResult<RespuestaAutenticacion>> Crear([FromBody] CredencialesUsuario credenciales)
+        {
+            var usuario = new IdentityUser { UserName = credenciales.Email, Email = credenciales.Email };
+            var resultado = await userManager.CreateAsync(usuario, credenciales.Password);
+
+            if (resultado.Succeeded)
+            {
+                return await ConstruirToken(credenciales);
+            }
+            else
+            {
+                return BadRequest(resultado.Errors);
+            }
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<RespuestaAutenticacion>> Login([FromBody] CredencialesUsuario credenciales)
+        {
+            var resultado = await signInManager.PasswordSignInAsync(credenciales.Email, credenciales.Password, isPersistent: false, lockoutOnFailure: false);
+            if(resultado.Succeeded)
+            {
+                return await ConstruirToken(credenciales);
+            }else
+            {
+                return BadRequest("Credenciales incorrectas");
+            }
+        }
+
+        private async Task<RespuestaAutenticacion> ConstruirToken(CredencialesUsuario credenciales)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("email", credenciales.Email)
+            };
+
+            var usuario = await userManager.FindByEmailAsync(credenciales.Email);
+            var claimsDB = await userManager.GetClaimsAsync(usuario);
+
+            claims.AddRange(claimsDB);
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["llavejwt"]));
+            var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
+
+            var expiracion = DateTime.UtcNow.AddYears(1);
+
+            var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expiracion, signingCredentials: creds);
+
+            return new RespuestaAutenticacion()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiracion = expiracion
+            };
+        }
+    }
+}
+
+
---
+++ back-end/DTO/RespuestaAutenticacion.cs
@@ -0,0 +1,8 @@
+﻿namespace back_end.DTO
+{
+    public class RespuestaAutenticacion
+    {
+        public string Token { get; set; }
+        public DateTime Expiracion { get; set; }
+    }
+}
